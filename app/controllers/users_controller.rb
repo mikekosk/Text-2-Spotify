2e626ecfc6754d428f72a964309dc49f6d12b2f0
Michael Kosk <michael@Michaels-MBP.T-mobile.com>
2018-05-23 21:45:27 -0500

    working ! but slow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,23 +3,49 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     spotify_user = RSpotify::User.new(session[:hash])
+
+    ## Select Playlist
     if params[:playlist_id]
-      playlist_id = params[:playlist_id].to_i
+      playlist_id = params[:playlist_id].to_i - 1
       @tracks = spotify_user.playlists[playlist_id].tracks(limit: 40)
     else
       @tracks = nil
     end
 
+    ## Sort Playlist
     if params[:sort_by] == 'name'
       @tracks = @tracks.sort_by(&:name)
     elsif params[:sort_by] == 'danceability'
       @tracks = @tracks.sort_by{|a| a.audio_features.danceability}
+    elsif params[:sort_by] == 'tempo'
+      @tracks = @tracks.sort_by{|a| a.audio_features.tempo}
+    elsif params[:sort_by] == 'valence'
+      @tracks = @tracks.sort_by{|a| a.audio_features.valence}
+    elsif params[:sort_by] == 'speechiness'
+      @tracks = @tracks.sort_by{|a| a.audio_features.speechiness}
+    elsif params[:sort_by] == 'instrumentalness'
+      @tracks = @tracks.sort_by{|a| a.audio_features.instrumentalness}
     else
       @tracks
     end
 
-    if params[:save] == true
-      spotify_user. 
+    ## Save Playlist
+    if params[:save] == 'true'
+      ## Pull playlist name & sort style
+      playlist_name = spotify_user.playlists[playlist_id].name
+      sort_style = params[:sort_by]
+
+      ## Create Playlist & Add Tracks
+      new_playlist = spotify_user.create_playlist!("#{playlist_name},
+        sorted by #{sort_style}")
+      new_playlist.add_tracks!(@tracks)
+
+      ## Redirect if successful
+      flash[:success] = "Save Successful"
+      redirect_to params.permit(:playlist_id, :sort_by)
+        .merge({:playlist_id => 1})
+    end
+
 
   end
 
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def sort_link(column, title = nil)
+    title ||= column.titleize
+
+    link_to "#{title} <span class='#{icon}'></span>".html_safe, {column: column, direction: direction}
+  end
 end
